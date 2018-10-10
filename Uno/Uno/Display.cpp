faa473b17894a9f642470af0ca581a4791a88f5a
Hancock <MH137428@davenportschools.org>
2018-10-10 11:06:35 -0500

    Update

--- Uno/Uno/Display.cpp
+++ Uno/Uno/Display.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include <Windows.h>
 #include "Header.h"
 using namespace std;
+using namespace std::chrono;
+using namespace std::this_thread;
 
 void setColor(char color) {
 	switch (color) {
 	case 'r':
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); //Red
 		break;
 	case 'g':
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10); //Green
 		break;
 	case 'y':
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14); //Yellow
 		break;
 	case 'b':
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11); //Blue
 		break;
+	default:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);  //White
 	}
+}
+
+void displayHand(int player) {
+	cout << "\nYour Hand: " << endl;
+	for (int i = 0; i < 7; i++)
+		if (i < 6) {
+			sleep_for(seconds(1));
+			//cout << playerCards[i] << ", ";
+		}
+		else {
+			sleep_for(seconds(1));
+			//out << playerCards[i] << endl;
+			setColor(0);
+		}
 }
